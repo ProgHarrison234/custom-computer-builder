3c4b9ed5780ac6e400fec6491edc170840ca633a
Kathryn Teall <katgrace0808@gmail.com>
2020-07-01 21:07:59 -0700

    Added schema tables and package.json

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,38 @@
 CREATE DATABASE custom_computer_db;
 USE custom_computer_db;
 
-CREATE TABLE computer
+CREATE TABLE gaming
 (
 	id int NOT NULL AUTO_INCREMENT,
-	item varchar(60) NOT NULL,
+	component varchar(60) NOT NULL,
+	section VARCHAR(20) NOT NULL,
     name varchar(100) NOT NULL,
     description varchar(255),
 	price INT NOT NULL,
+	rating VARCHAR(10) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE mediacenter
+(
+	id int NOT NULL AUTO_INCREMENT,
+	component varchar(60) NOT NULL,
+	section VARCHAR(20) NOT NULL,
+    name varchar(100) NOT NULL,
+    description varchar(255),
+	price INT NOT NULL,
+	rating VARCHAR(10) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE workstation
+(
+	id int NOT NULL AUTO_INCREMENT,
+	component varchar(60) NOT NULL,
+	section VARCHAR(20) NOT NULL,
+    name varchar(100) NOT NULL,
+    description varchar(255),
+	price INT NOT NULL,
+	rating VARCHAR(10) NOT NULL,
 	PRIMARY KEY (id)
 );
