5b3ee515e67b02f3fb0b0f8c3d1b308348d3c960
Kathryn Teall <katgrace0808@gmail.com>
2020-07-01 18:40:04 -0700

    File shells created

---
+++ server.js
@@ -0,0 +1,20 @@
+let express = require("express");
+
+let app = express();
+let PORT = process.env.PORT || 8080;
+
+let db = require("./models");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
