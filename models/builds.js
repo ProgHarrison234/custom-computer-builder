592cdb575939657a252733579f25cb1ebba3ee75
Kathryn Teall <katgrace0808@gmail.com>
2020-07-11 10:48:28 -0700

    Updated builds model for saving chosen builds

--- models/builds.js
+++ models/builds.js
@@ -1,10 +1,34 @@
 module.exports = function (sequelize, DataTypes) {
     let Builds = sequelize.define("Builds", {
-        part_id: {
+        CPU_part_id: {
             type: DataTypes.INTEGER,
             allowNull: false
         },
-        id_1: {
+        CPU_cooler_part_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        Motherboard_part_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        RAM_part_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        Storage_part_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        GPU_part_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        PSU_part_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        Case_part_id: {
             type: DataTypes.INTEGER,
             allowNull: false
         }
